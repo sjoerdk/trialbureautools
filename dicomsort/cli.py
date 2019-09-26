f0696bbcfa168afa92fa36587b5e7de809090764
Sjoerd Kerkstra <sjoerd.kerkstra@radboudumc.nl>
2019-09-26 13:39:46 +0200

    First steps

---
+++ dicomsort/cli.py
@@ -0,0 +1,24 @@
+"""Command line interface for dicomsort
+
+"""
+
+class DicomSortSettings:
+    """All settings needed for dicomsort
+
+    """
+    def __init__(self, patterns, active_pattern):
+        """
+
+        Parameters
+        ----------
+        patterns:
+        active_pattern
+        """
+
+
+class DicomSortContext:
+    """Context object passed to dicomsort click commands
+
+    """
+
+    def __init__(self, dicom):
